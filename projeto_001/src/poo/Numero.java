be5fd9a46d129ee084dbe0034c1f4fe3a31f62c4
marcelo-ls-reis <marcelo.reis5@fatec.sp.gov.br>
2022-09-08 19:23:22 +0000

    08/09/2022

---
+++ projeto_001/src/poo/Numero.java
@@ -0,0 +1,8 @@
+package poo;
+
+public class Numero{
+    public int x = 10;
+    public int soma(int valor){
+        return valor = valor + x;
+    }
+}
---
+++ projeto_001/src/poo/TesteNumero.java
@@ -0,0 +1,10 @@
+package poo;
+
+public class TesteNumero {
+    public static void main(String[] args) {
+        Numero numero = new Numero();
+        int resultado = numero.soma(20);
+        System.out.println("O Resultado da soma é: " + resultado);        
+        
+    }
+}
