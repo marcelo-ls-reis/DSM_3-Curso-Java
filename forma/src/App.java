60886fbfd5aa022aac438a3bfb7b86ad96ffa515
marcelo-ls-reis <marcelo.reis5@fatec.sp.gov.br>
2022-10-24 20:41:30 +0000

    Get and Set

--- forma/src/App.java
+++ forma/src/App.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
--- forma/src/Forma.java
+++ forma/src/Forma.java
@@ -1,4 +1,4 @@
-import java.util.function.Function;
+import java.util.*;
 
 public abstract class Forma { 
 
@@ -8,4 +8,27 @@ public abstract class Forma {
 
     public abstract float calcularArea();
     public abstract float calcularPerimetro();
+
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    public float getLargura() {
+        return largura;
+    }
+    public void setLargura(float largura) {
+        this.largura = largura;
+    }
+    public float getRaio() {
+        return raio;
+    }
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+    Random Valor = new Random();
+        raio = Valor.nextInt(50);
+
+    
 }
